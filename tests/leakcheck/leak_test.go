4605a5c51fafc1acb73a248d73891dff493ba269
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-18 12:50:13 -0700

    Release test context references after cleanup

--- tests/leakcheck/leak_test.go
+++ tests/leakcheck/leak_test.go
@@ -54,7 +54,6 @@ var objectLeakOpts = []objectleak.Option{
 	objectleak.WithPruneType("google.golang.org/protobuf/internal/impl.*"),
 	// TODO: TestEnv dependencies remain retained while teardown fixes are reverted.
 	objectleak.WithExclude("FunctionalTestBase*"),
-	objectleak.WithExclude("ctx*"),
 	objectleak.WithExclude("sdkClient*"),
 }
 
